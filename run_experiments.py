63327e80d3bfa7031519fd0ae9aa6d11ab522e03
Michał Łuszczyk <mluszczyk@users.noreply.github.com>
2018-03-24 21:42:42 +0100

    Experiment: test hidden layers.

--- run_experiments.py
+++ run_experiments.py
@@ -27,24 +27,49 @@ SERIES_NAME = datetime.datetime.now().strftime("%y-%m-%d-%H%M%S")
 SERIES_DIR = os.path.join(PROJ_DIR, "series", SERIES_NAME)
 
 
+def run_process(exp_name, opts):
+    path = os.path.join(SERIES_DIR, exp_name)
+    os.makedirs(path)
+    sbatch_path = os.path.join(path, "sat-" + exp_name)
+
+    opts_string = ' '.join(["{}={}".format(name, value) for name, value in opts.items()])
+
+    with open(sbatch_path, "w") as f:
+        f.write(SBATCH_TEMPLATE.format(options=opts_string))
+    subprocess.run(["sbatch", sbatch_path], check=True, cwd=path)
+
+
 def main():
     CLAUSE_SIZE = 3
-    EMBEDDING_SIZE = 128 + 64
     LEARNING_RATE = 0.001
-    for VARIABLE_NUM in [4, 5, 6, 7, 8, 9]:
-        CLAUSE_NUM = VARIABLE_NUM * 5
+    VARIABLE_NUM = 8
+    CLAUSE_NUM = VARIABLE_NUM * 5
+    SAMPLES = 4 * VARIABLE_NUM * 10 ** 6
+    EMBEDDING_SIZE = 256
+    LSTM_STATE_SIZE = 128
 
-        exp_name = "var{}".format(VARIABLE_NUM)
-        path = os.path.join(SERIES_DIR, exp_name)
-        os.makedirs(path)
-        SAMPLES = 2 * VARIABLE_NUM * 10 ** 6
-        opts = ["VARIABLE_NUM", "CLAUSE_NUM", "EMBEDDING_SIZE", "LEARNING_RATE", "SAMPLES", "CLAUSE_SIZE"]
+    opts = ["VARIABLE_NUM", "CLAUSE_NUM", "EMBEDDING_SIZE", "LEARNING_RATE", "SAMPLES", "CLAUSE_SIZE",
+            "SAT_HIDDEN_LAYERS", "POLICY_HIDDEN_LAYERS",
+            "SAT_HIDDEN_LAYER_SIZE", "POLICY_HIDDEN_LAYER_SIZE"
+    ]
+
+    # baseline
+    SAT_HIDDEN_LAYERS = 0
+    POLICY_HIDDEN_LAYERS = 0
+    POLICY_HIDDEN_LAYER_SIZE = SAT_HIDDEN_LAYER_SIZE = 0  # ignored anyway
+    locals_ = locals()
+    opts = {key: locals_[key] for key in opts}
+    run_process("baseline", opts)
+
+    SAT_HIDDEN_LAYERS = 1
+    POLICY_HIDDEN_LAYERS = 1
+    for POLICY_HIDDEN_LAYER_SIZE in [2 * VARIABLE_NUM, 4 * VARIABLE_NUM, 8 * VARIABLE_NUM,
+                                     EMBEDDING_SIZE // 2, EMBEDDING_SIZE]:
+        exp_name = "hid{}".format(POLICY_HIDDEN_LAYER_SIZE)
+        SAT_HIDDEN_LAYER_SIZE = POLICY_HIDDEN_LAYER_SIZE
         locals_ = locals()
-        opts_string = ' '.join(["{}={}".format(name, locals_[name]) for name in opts])
-        sbatch_path = os.path.join(path, "sat-" + exp_name)
-        with open(sbatch_path, "w") as f:
-            f.write(SBATCH_TEMPLATE.format(options=opts_string))
-        subprocess.run(["sbatch", sbatch_path], check=True, cwd=path)
+        opts = {key: locals_[key] for key in opts}
+        run_process(exp_name, opts)
 
 
 if __name__ == '__main__':
