c1823b6ea4b2a99d29cda1a0bf4122159617f3ad
Michał Łuszczyk <mluszczyk@users.noreply.github.com>
2018-10-17 13:58:57 +0200

    Run experiments for LSTM+SR.

--- run_experiments.py
+++ run_experiments.py
@@ -37,37 +37,36 @@ def run_process(exp_name, opts):
 
 
 def main():
-    CLAUSE_SIZE = 3
     LEARNING_RATE = 0.001
-    VARIABLE_NUM = 8
+    VARIABLE_NUM = 7
     EMBEDDING_SIZE = 256
     LSTM_STATE_SIZE = 128
     BATCH_SIZE = 128
     POLICY_HIDDEN_LAYERS = 1
     SAT_HIDDEN_LAYERS = 1
-    VARIABLE_NUM = 10
     SR_GENERATOR = True
     NEPTUNE_ENABLED = True
+    LSTM_LAYERS = 1
 
     opts = [
         "VARIABLE_NUM", "CLAUSE_NUM", "EMBEDDING_SIZE",
         "LEARNING_RATE", "SAMPLES", "CLAUSE_SIZE", "BATCH_SIZE",
         "POLICY_HIDDEN_LAYERS", "POLICY_HIDDEN_LAYER_SIZE",
         "SAT_HIDDEN_LAYERS", "SAT_HIDDEN_LAYER_SIZE",
-        "LSTM_LAYERS", "LSTM_STATE_SIZE", "NEPTUNE_ENABLED"
+        "LSTM_LAYERS", "LSTM_STATE_SIZE", "NEPTUNE_ENABLED",
+        "SR_GENERATOR"
     ] 
 
-    for VARIABLE_NUM in [5]:
-        for LSTM_LAYERS in [1]:
-            CLAUSE_NUM = VARIABLE_NUM * 5
-            SAMPLES = 4 * VARIABLE_NUM * 10 ** 6
-            POLICY_HIDDEN_LAYER_SIZE = 4 * VARIABLE_NUM
-            SAT_HIDDEN_LAYER_SIZE = 4 * VARIABLE_NUM
+    CLAUSE_SIZE = VARIABLE_NUM
+    CLAUSE_NUM = VARIABLE_NUM * 5
+    SAMPLES = 4 * VARIABLE_NUM * 10 ** 6
+    POLICY_HIDDEN_LAYER_SIZE = 4 * VARIABLE_NUM
+    SAT_HIDDEN_LAYER_SIZE = 4 * VARIABLE_NUM
 
-            exp_name = "lstm{}-varnum{}".format(LSTM_LAYERS, VARIABLE_NUM)
-            locals_ = locals()
-            opts = {key: locals_[key] for key in opts}
-            run_process(exp_name, opts)
+    exp_name = "sr-lstm{}-varnum{}".format(LSTM_LAYERS, VARIABLE_NUM)
+    locals_ = locals()
+    opts = {key: locals_[key] for key in opts}
+    run_process(exp_name, opts)
 
 
 if __name__ == '__main__':
