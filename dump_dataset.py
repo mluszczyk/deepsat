74e3c0a34c73406e44e7f2284f3b23f9bcecd4e2
Henryk Michalewski <henrykmichalewski@gmail.com>
2018-12-27 22:01:54 +0000

    Tqdm

--- dump_dataset.py
+++ dump_dataset.py
@@ -2,6 +2,7 @@ import tensorflow as tf
 import numpy as np
 
 import cnf_dataset
+from tqdm import tqdm
 
 
 def make_example(inputs, sat, policy):
@@ -25,7 +26,7 @@ def tf_serialize_example(sample):
 def main():
     filename = "test.tfrecord"
     options = {
-        "PROCESSOR_NUM": 4,
+        "PROCESSOR_NUM": 80,
         "CLAUSE_NUM": 80,
         "VARIABLE_NUM": 8,
         "MIN_VARIABLE_NUM": 1,
@@ -37,7 +38,7 @@ def main():
     with cnf_dataset.PoolDatasetGenerator(options) as generator, \
             tf.python_io.TFRecordWriter(filename) as writer:
 
-        for _ in range(n_observations):
+        for _ in tqdm(range(n_observations)):
             sample_with_labels = generator.generate_batch()
             tf_sample = {
                  "inputs": np.squeeze(sample_with_labels.inputs.astype(np.float32), 0),
